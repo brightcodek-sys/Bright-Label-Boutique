9ddec2dc0cbfbd62f6fac55d95478be6c63fa130
brightcodek-sys <brightcodek@gmail.com>
2026-03-21 11:25:28 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const CONFIG = {
+    SUPABASE_URL: 'https://NEW_PROJECT_REF.supabase.co', // Get this from Settings > API
+    SUPABASE_KEY: 'NEW_ANON_KEY_HERE'                 // Get this from Settings > API
+};
